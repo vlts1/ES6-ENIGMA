3ee32b2d07931e68ed9d2f13458a9ad1d6b2a5e1
vlts1 <vladyslavtsepenok@gmail.com>
2023-02-21 21:55:17 -0800

    Save File button

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let openFiles = [];
 let lastOpenFileName = "";
+
 class File {
     constructor(name, content) {
         if (!name.endsWith(".css") && !name.endsWith(".html") && !name.endsWith(".js")) {
@@ -91,6 +92,61 @@ function saveFileTextChange() {
         const file = openFiles[fileId];
         if (file.name === lastOpenFileName) {
             file.content = newTextValue;
+        }
+    }
+}
+
+function download(strData, strFileName, strMimeType) {
+    var D = document,
+        A = arguments,
+        a = D.createElement("a"),
+        d = A[0],
+        n = A[1],
+        t = A[2] || "text/plain";
+
+    //build download link:
+    a.href = "data:" + strMimeType + "charset=utf-8," + escape(strData);
+
+
+    if (window.MSBlobBuilder) { // IE10
+        var bb = new MSBlobBuilder();
+        bb.append(strData);
+        return navigator.msSaveBlob(bb, strFileName);
+    } /* end if(window.MSBlobBuilder) */
+
+
+
+    if ('download' in a) { //FF20, CH19
+        a.setAttribute("download", n);
+        a.innerHTML = "downloading...";
+        D.body.appendChild(a);
+        setTimeout(function() {
+            var e = D.createEvent("MouseEvents");
+            e.initMouseEvent("click", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+            a.dispatchEvent(e);
+            D.body.removeChild(a);
+        }, 66);
+        return true;
+    }; /* end if('download' in a) */
+
+
+
+    //do iframe dataURL download: (older W3)
+    var f = D.createElement("iframe");
+    D.body.appendChild(f);
+    f.src = "data:" + (A[2] ? A[2] : "application/octet-stream") + (window.btoa ? ";base64" : "") + "," + (window.btoa ? window.btoa : escape)(strData);
+    setTimeout(function() {
+        D.body.removeChild(f);
+    }, 333);
+    return true;
+}
+
+function downloadOpen() {
+    for (let fileId = 0; fileId < openFiles.length; fileId++) {
+        const file = openFiles[fileId];
+        if (file.name === lastOpenFileName) {
+            download(file.content, file.name, 'text/plain');
+            break;
         }
     }
 }
